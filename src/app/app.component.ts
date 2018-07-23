8a59b844560c46c0ef4e3ab0655dc9290853d17d
Prawit <maxganiget_2539@hotmail.com>
2018-07-23 13:32:17 +0700

    init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:string = 'My Angular App';
+  version:Number = 1.0;
+  disable:Boolean = true;
+
+  changeTitle(event) {
+    console.log(event.target.value);
+    this.title = event.target.value;
+  }
+
+}
